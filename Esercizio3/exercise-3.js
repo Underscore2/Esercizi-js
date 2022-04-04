b5631be39cdc28f4d87ea01fcaf1ed427277fa9f
Underscore2 <99175453+Underscore2@users.noreply.github.com>
2022-04-04 16:25:20 +0200

    feat: add 1-8 exercise

---
+++ Esercizio3/exercise-3.js
@@ -0,0 +1,10 @@
+const students = ['Paul', 'George', 'Lucas'];
+
+function addStudent(student) {
+  // ...
+  students.push(student)
+}
+
+addStudent('Marco');
+addStudent('Giovanni');
+console.log(students);
